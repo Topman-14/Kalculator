69bd549e387c760f55e48795c17c347e7a2a8de8
Topman-14 <talktotimothy101@gmail.com>
2022-11-10 14:10:29 +0100

    Almost complete

--- Kalc.js
+++ Kalc.js
@@ -1,9 +1,30 @@
 const keys = document.querySelectorAll("button");
+const ac = document.querySelector("#ac");
+const del = document.querySelector("#del");
 
 for (let i = 0; i<keys.length; i++){
    keys[i].addEventListener("click", function(){
       document.querySelector('#operation').textContent += keys[i].value;
-      console.log(Math.eval(document.querySelector('#operation').textContent))
-
+      try{
+         document.querySelector("#result").textContent = eval(document.querySelector('#operation').textContent)
+      }
+      catch{
+         document.querySelector("#result").textContent = "null"
+      }
    })
 }
+
+del.addEventListener("click", ()=>{
+   document.querySelector('#operation').textContent = document.querySelector('#operation').textContent.slice(0, -1);
+   try{
+      document.querySelector("#result").textContent = eval(document.querySelector('#operation').textContent);
+   }
+   catch{
+      document.querySelector("#result").textContent = "null";
+   }
+});
+
+ac.addEventListener('click', ()=>{
+   document.querySelector('#operation').textContent = ""
+   document.querySelector("#result").textContent = ""
+})
